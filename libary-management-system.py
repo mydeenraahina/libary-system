b68bbe844fd038d2e2d1628f9370d11ee1335022
MydeenRaahina <145376500+mydeenraahina@users.noreply.github.com>
2023-11-22 23:46:13 -0500

    Update libary-management-system.py

--- libary-management-system.py
+++ libary-management-system.py
@@ -11,49 +11,51 @@ def menu():
     print(f"DAY :{day_of_week}")
     
     menu_listed=["MENU","1.UPDATING NEW ARRAIVAL","2.checking_availability_OF BOOK",
-                 "3.counting_targeted_book_avalability",
-                 "4.borrow_book",
-                 "5.display available books"]
+                 "3.borrow_book",
+                 "4.display available books"]
     print("----FINE AMOUNT SHOULD BE COLLECTED IF DUE DATE EXIT----")
     
     for attempt in menu_listed:
         print(attempt)
         
 menu()
-
+list_value=()
 class libary_system:
-    @staticmethod
-    def main_block():
-        def new_arraival(*args):
-            list_value.extend(args)
+    def new_arraival(self,*args):
+        global list_value
+        list_value+=(args)
+        print(list_value)
             
-        def checking_availability(expected_book):
-            if expected_book in list :
-                print(f"{expected_book} is still available")
+    def checking_availability(self,expected_book):
+        if expected_book in list_value :
+            print(f"{expected_book} is still available")
+        else:
+            print(f"sorry!\n {expected_book} is no available")
+    
+            
+    def borrow_book(self):
+        while True:
+            book_name=input(f"Enter your needed_book name :")
+            if book_name.isalpha():
+                current_date=date.today()
+                last_date_of_submission=date.today()+timedelta(days=10)
+                print(f"last date of your book {[book_name]} submision ={last_date_of_submission}")
+                break
             else:
-                print(f"sorry!\n {expected_book} is no available")
-                
-        def counting_targeted_book_avalability(target_book):
-            print(f"total_availability_of_book_in_no: {list.count(target_book)}")
-            
-        def borrow_book():
-            while True:
-                book_name=input(f"Enter your needed_book name :")
-                if book_name.isalpha():
-                    current_date=date.today()
-                    last_date_of_submission=date.today()+timedelta(days=10)
-                    print(f"last date of your book {[book_name]} submision ={last_date_of_submission}")
-                    break
-                else:
-                    print(f"Enter a valid input")
+                print(f"Enter a valid input")
 
-        def display_all_available_books():
+    def display_all_available_books(self):
+        if not list_value:
+            print("Book list is empty!!!")
+        else:
+            print("Available books:")
             for i in list_value :
                 print(i)
-        list_value=[]
+class main_block:
+    def __init__(self):
         while True:        
             getting_input=input(f"Enter your choice :")
-                
+                        
             if getting_input=="1":
                 while True:
                     try:
@@ -62,42 +64,52 @@ class libary_system:
                         break
                     except ValueError:
                         print("enter a valid input")
-                        
+                                
                 for i in range(1,no_of_new_arraival+1):
                     getting_books_arrived=input("Enter the name of newly arrived book")
                     if getting_books_arrived.isalpha():
                         newy_arraival.append(getting_books_arrived)
-                        print(f"New arraivals updated successfully")
                     else:
                         print(f"Enter a valid input")
+                print(f"New arraivals updated successfully")
+                                        
                                 
-                        
                 value=",".join(newy_arraival)
-                new_arraival(value)
+                obj=libary_system()
+                obj.new_arraival(value)
                 break
-                        
+                                
             elif getting_input=="2":
                 availability_of_book=input(f"Enter book name to check availability")
-                checking_availability(availability_of_book)
+                obj1=libary_system()
+                obj1.checking_availability(availability_of_book)
                 break
-                
+                        
             elif getting_input=="3":
-                target_book=input(f"Enter the book to count the availability :")
-                counting_targeted_book_avalability(target_book)
+                obj3=libary_system()
+                obj3.borrow_book()
                 break
-                
+                        
             elif getting_input=="4":
-                borrow_book()
+                obj4=libary_system()
+                obj4.display_all_available_books()
                 break
-                
-            elif getting_input=="5":
-                display_all_available_books()
-                break
-                
+                        
             else:
                 print(f"Enter a valid input")
-            
-libary_system.main_block()
+object=main_block()
+while True:
+    getting_input_for_continue=input("Do you want to continue process [yes\\no] ")
+    if getting_input_for_continue.casefold()=="yes":
+        object1=main_block()
+        break
+    elif getting_input_for_continue.casefold()=="no":
+        print("Exit!")
+        break
+    else:
+        print("Enter a valid input")
+        
 
 
-    
+                
+
